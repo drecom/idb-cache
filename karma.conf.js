e6785d7f790bce3234f1a2a853c9b35bef81397e
eight <eight04@gmail.com>
2019-04-01 19:04:38 +0800

    Add: puppeteer dep

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai'],
@@ -6,4 +8,4 @@ module.exports = function(config) {
     reporters: ['mocha'],
     singleRun: true,
   })
-}
+};
