24b599a30ee4b64671821ad5ed1e35f0251dc848
zprodev <zproject.develop@gmail.com>
2019-06-04 19:40:38 +0900

    Abolish new features of ES

--- src/utils/canUseBlob.ts
+++ src/utils/canUseBlob.ts
@@ -8,9 +8,9 @@ export default function canUseBlob(): boolean {
       const iosVer = iosVerRegexResult[1];
       const iosVerNums = iosVer.split('_');
       const iosVerStr
-        = ((iosVerNums[0] === undefined) ? '000' : iosVerNums[0].padStart(3, '0')) // major
-        + ((iosVerNums[1] === undefined) ? '000' : iosVerNums[1].padStart(3, '0')) // minor
-        + ((iosVerNums[2] === undefined) ? '000' : iosVerNums[2].padStart(3, '0')) // patch
+        = ((iosVerNums[0] === undefined) ? '000' : ('000' + iosVerNums[0]).slice(-3))  // major
+        + ((iosVerNums[1] === undefined) ? '000' : ('000' + iosVerNums[1]).slice(-3))  // minor
+        + ((iosVerNums[2] === undefined) ? '000' : ('000' + iosVerNums[2]).slice(-3)); // patch
       // Less than iOS12.2 can not use blob
       if (iosVerStr < '012002000') {
         return false;
